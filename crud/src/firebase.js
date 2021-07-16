317cef822e5c1db9582a017a93c89ee1e280641a
subhadip007 <63991623+subhadip007@users.noreply.github.com>
2021-07-16 12:05:51 +0530

    fixed

--- crud/src/firebase.js
+++ crud/src/firebase.js
@@ -1,17 +1,30 @@
-import firebase from 'firebase'
+
+import firebase from "firebase";
+
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBZGQUgkaPyxrNN2AC5ZmtmACl__gabCy8',
-  authDomain: 'crud-1d475.firebaseapp.com',
-  databaseURL:
-		'https://crud-1d475-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'crud-1d475',
-  storageBucket: 'crud-1d475.appspot.com',
-  messagingSenderId: '146210841885',
-  appId: '1:146210841885:web:68095212eb181ea857edf4',
-  measurementId: 'G-BVVE8S8MHZ'
-}
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig)
+  apiKey: "AIzaSyBZxsDdckwC10nWjfiF0JYC0dL4lyR4zVg",
+  authDomain: "kiflon.firebaseapp.com",
+  databaseURL: 'https://kiflon-default-rtdb.firebaseio.com/',
+  projectId: "kiflon",
+  storageBucket: "kiflon.appspot.com",
+  messagingSenderId: "916936170229",
+  appId: "1:916936170229:web:7a1b95200e5c511af283bc"
 
-export default firebase
+}
+
+
+
+
+if (!firebase.apps.length) {
+   firebase.initializeApp(firebaseConfig);
+}else {
+   firebase.app(); 
+}
+
+
+
+
+export  default firebase;
+
+
